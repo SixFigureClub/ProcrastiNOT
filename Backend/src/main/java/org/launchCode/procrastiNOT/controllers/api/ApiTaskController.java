b08a33566d51dadd4e633d16385f1e14e9745aa0
rinashovik <rinashovik@gmail.com>
2024-01-10 20:56:44 -0600

    Created the routing for browsering the page and connected to BackEnd

--- Backend/src/main/java/org/launchCode/procrastiNOT/controllers/api/ApiTaskController.java
+++ Backend/src/main/java/org/launchCode/procrastiNOT/controllers/api/ApiTaskController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins="http://localhost:8080", maxAge = 3600)
+@CrossOrigin(origins="http://localhost:3000", maxAge = 3600)
 @RestController
 @RequestMapping("/api/task")
 public class ApiTaskController {
