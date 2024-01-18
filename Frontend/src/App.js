37cb2e4b9aac7c341df829cd3022ac8203304355
rinashovik <rinashovik@gmail.com>
2024-01-18 12:50:07 -0600

    Added new feature and updated CRUD functionalities

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -13,6 +13,7 @@ import AddTask from './Components/tasks/AddTask';
 import Search from './Components/Search'
 import TaskList from './Components/tasks/TaskList';
 import EditTask from './Components/tasks/EditTask';
+import DropColumn from './Components/tasks/DropColumn'
 
 
 
@@ -28,20 +29,18 @@ function App() {
 
       <Routes>
 
-        {/* <Route path="/" element={<HomePage />} /> */}
-
-
+        <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/register" element={<RegisterForm />} />
-        {/* <Route path="/welcome" element={<NavBar />} /> */}
         <Route  path="/list" element={<TaskList />} />
 
 
         {/* Don't use HomePage component */}
         <Route path="/addtask" element={<AddTask />} />
+        <Route path='/task/:id' element={<EditTask />} />
+        <Route path="/action" element={<DropColumn />} />
         <Route path="/search" element={<Search />} />
-        <Route path='/edit/{id}' element={<EditTask />} />
-        {/* <Route path="/newday" element={<ToWrapper />} /> */}
+
 
 
       </Routes>
@@ -51,31 +50,3 @@ function App() {
 
 export default App;
 
-
-// import './App.css';
-// import { BrowserRouter as Router, Route } from "react-router-dom";
-// import Navbar from "./Components/welcomePage/Navbar"
-// import LoginForm from './Components/LoginForm/LoginForm';
-// import RegisterForm from './Components/RegisterForm/RegisterForm';
-// import HomePage from './Components/HomePage/HomePage';
-// import WelcomePage from './Components/welcomePage/WelcomePage';
-// import LogoutButton from './Components/HomePage/LogoutButton';
-// function App() {
-//   return (
-//     <Router>
-//       <Navbar />
-//       {/* <Switch> */}
-//         <Route path='/'  component={WelcomePage} />
-//         <Route path='/login' component={LoginForm} />
-//         <Route path='/register' component={RegisterForm} />
-//         <Route path='/' component={LogoutButton} />
-//        <Route path="/" element={<HomePage />} />
-
-
-//         {/* <Route path='/addtask' component={AddTask} /> */}
-//       {/* </Switch> */}
-//     </Router>
-//   );
-// }
-
-// export default App;
--- Frontend/src/Components/tasks/EditTask.jsx
+++ Frontend/src/Components/tasks/EditTask.jsx
@@ -1,8 +1,78 @@
 
 
+// import React, {useState, useEffect} from 'react'
+// import axios from 'axios';
+// import { useNavigate, useParams } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+
+
+
+// export default function EditTask() {
+
+//       let {id} = useParams();
+//    let navigate = useNavigate()
+
+//         const [task, setTask] = useState({})
+
+//         // const tasks = task;
+
+//         useEffect(()=>{
+//           // console.log("Coding with Rina");
+//           loadTasks();
+
+//         }, [])
+    
+//         const loadTasks=async()=>{
+    
+//               const results =await axios.get("http://localhost:8080/login/task/${id}")
+                        
+//                 console.log(results.data);
+//                 setTask(results.data)
+    
+//         };
+        
+        
+//         const onInputChange=(e)=>{
+//           e.persist();
+//           // setTask(e.target.value);
+//           setTask({...task, [e.target.name]:e.target.value});
+
+
+
+//           };
+           
+
+//         const handleUpdateSubmit = async(e) => {
+//             e.preventDefault();
+
+//            console.log(task);
+//             setTask(data);
+
+//       const data = {name:task.description}
+
+
+//  await axios.put(`http://localhost:8080/login/task/${id}`,data)
+//    .then(res => {
+
+//     console.log(res);
+
+//   alert(res.data.message);
+
+
+//  });
+//  navigate("/list")
+
+        
+//         }
+
+
+
+
+
 import React, {useEffect, useState} from 'react'
 import axios from 'axios';
 import { useNavigate, useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
   export default function EditTask () {
@@ -27,40 +97,53 @@ useEffect (() => {loadTasks()}, [])
 
  await axios.put(`http://localhost:8080/login/task/${id}`,task) 
 
- navigate("/welcome")
-          // setTask("")
-        
+ navigate("/list")        
       
         };
 
         const loadTasks=async()=>{
 
             const results =await axios.get(`http://localhost:8080/login/task/${id}`)
-            //  console.log(results.data);
               setTask(results.data)
   
       };
 
+  return (
+          
+
+      <div className='container mt-5'>
+      <div className='row'>
+      <div className='col-md-12'>
+      <div className='card'>
+      <div className='card-header'>
+        <h2>Edit Task  
+        <Link to="/list" className='btn btn-warning brn-lg float-end'>Back</Link>
+      </h2>
+
+      </div>
+      <div className='card-body'>
+
+                <form onSubmit={handleSubmit}>
+                  <div className='mb-3'>
+      <input className='form-control' type="text" name="description"  value={task.description}  onChange={(e) => onInputChange(e)} />
+
+      <button className="btn btn-outline-primary"  type="submit">Update Task</button>
+
+      </div>
+
+      </form>
+
+      </div>
+
+      </div>
+
+      </div>
+
+      </div>
+
+      </div>
+        )
+}
 
 
 
-          return (
-<div>
-
-<h1> Editing the</h1>
-
-
-            <div  className='container mt-5'>
-              {/* <div className='card'> */}
-                
-        <form onSubmit={(e) => handleSubmit(e)}>
-
-        <input   type={"text"} className="form-control"  name="description"  placeholder='try typing' onChange={(e) => onInputChange(e)} />
-        <button className="btn btn-primary" type="submit">Submit</button>
-        
-        </form>
-        </div>
-</div>
-
-          )
-        }
